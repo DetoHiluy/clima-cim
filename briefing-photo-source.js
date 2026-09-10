6af064f996854dbd15d5c9394818f5d7a2a28bb1
DetoHiluy <101139126+DetoHiluy@users.noreply.github.com>
2026-09-10 17:56:27 -0300

    Neutralizar antiga camada intermediária de imagem

---
+++ briefing-photo-source.js
@@ -0,0 +1,5 @@
+(() => {
+  'use strict';
+  // Compatibilidade temporária: o briefing visual carrega a fotografia limpa diretamente.
+  // Este arquivo não intercepta fetch, não injeta imagens e não altera o gerador.
+})();
